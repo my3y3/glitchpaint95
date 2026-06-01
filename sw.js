32a82a3e4aa1b48b8e615d97f74620d1c7160c74
My 3y3 <shengchew81@gmail.com>
2026-06-02 00:57:12 +0100

    Add caching for service worker installation and fetch
    Implement caching strategy for service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE = "glitchpaint-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
